1d986ffb9d8f0a3827277e2f4b7ee0140fb113e8
unknown <Przemyslaw Drygas>
2025-12-13 15:31:19 +0100

    Dodanie konfiguracji Dockera

--- backend/src/main/java/com/focusfortress/repository/UserRepository.java
+++ backend/src/main/java/com/focusfortress/repository/UserRepository.java
@@ -1,11 +1,14 @@
 package com.focusfortress.repository;
 
+import com.focusfortress.model.Role;
 import com.focusfortress.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
     Optional<User> findByVerificationToken(String token);
     boolean existsByEmail(String email);
+    void deleteByRoleAndCreatedAtBefore(Role role, LocalDateTime cutoff);
 }
